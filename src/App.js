0a2306444b4bd0fa14f0e5e56e3a4612b1c86e2e
Ruban Siva <rubansiva@hotmail.com>
2019-11-14 01:02:02 +0000

    update project

--- src/App.js
+++ src/App.js
@@ -1,81 +1,20 @@
 import React, { useState, Fragment } from 'react'
-import UserForm from './forms/UserForm'
-import UserTable from './tables/UserTable'
+import { Nav } from 'react-bootstrap';
+import Users from './users'
+
 
 const App = () => {
-	// Data
-	const usersData = [
-		{ id: 1, name: 'Tania', username: 'floppydiskette' },
-		{ id: 2, name: 'Craig', username: 'siliconeidolon' },
-		{ id: 3, name: 'Ben', username: 'benisphere' },
-	]
-
-	const initialFormState = { id: null, name: '', username: '' }
-
-	// Setting state
-	const [ users, setUsers ] = useState(usersData)
-	const [ currentUser, setCurrentUser ] = useState(initialFormState)
-	const [ userViewMode, setViewMode ] = useState("view")
-
-	// CRUD operations
-	const addUser = () => {
-		setViewMode("new")
-	}
-
-	const deleteUser = id => {
-		setUsers(users.filter(user => user.id !== id))
-	}
-
-	const updateUser = (id, updatedUser) => {
-		setUsers(users.map(user => (user.id === id ? updatedUser : user)))
-	}
-
-	const editUser = user => {
-		setViewMode("edit")
-		setCurrentUser({ id: user.id, name: user.name, username: user.username })
-	}
-
 	return (
 		<div className="container">
 			<h1>Book Library</h1>
-			<div className="flex-row">
-				<div className="flex-large">
-				{(userViewMode == "edit") ? (
-						<Fragment>
-							<h2>Edit user</h2>
-							<UserForm
-								editing={true}
-								setViewMode={setViewMode}
-								currentUser={currentUser}
-								updateUser={updateUser}
-							/>
-				</Fragment> ) : 
-					(userViewMode == "new") ? 
-						(
-							<Fragment>
-								<h2>Create user</h2>
-								<UserForm
-									editing={false}
-									setViewMode={setViewMode}
-									currentUser={initialFormState}
-									updateUser={updateUser}
-								/>
-					</Fragment> ) :
-						(
-							<Fragment>
-								<h2>View users</h2>
-								<div style={{ display: "flex", justifyContent: "flex-end" }}>
-									<button
-										onClick={() => addUser()}
-										className="button muted-button"
-									>
-										Add New User
-									</button>
-								</div>
-								<UserTable users={users} editRow={editUser} deleteUser={deleteUser} />
-						</Fragment> )}
-				</div>
-			</div>
+			<Nav variant="pills" defaultActiveKey="/users">
+				<Nav.Item>
+					<Nav.Link eventKey="/users">Users</Nav.Link>
+				</Nav.Item>
+				<Nav.Item>
+					<Nav.Link eventKey="/books">Books</Nav.Link>
+				</Nav.Item> 
+			</Nav>
 		</div>
 	)
 }
---
+++ src/users.js
@@ -0,0 +1,82 @@
+import React, { useState, Fragment } from 'react'
+import UserForm from './forms/UserForm'
+import UserTable from './tables/UserTable'
+
+const Users = () => {
+	// Data
+	const usersData = [
+		{ id: 1, name: 'Tania', username: 'floppydiskette' },
+		{ id: 2, name: 'Craig', username: 'siliconeidolon' },
+		{ id: 3, name: 'Ben', username: 'benisphere' },
+	]
+
+	const initialFormState = { id: null, name: '', username: '' }
+
+	// Setting state
+	const [ users, setUsers ] = useState(usersData)
+	const [ currentUser, setCurrentUser ] = useState(initialFormState)
+	const [ userViewMode, setViewMode ] = useState("view")
+
+	// CRUD operations
+	const addUser = () => {
+		setViewMode("new")
+	}
+
+	const deleteUser = id => {
+		setUsers(users.filter(user => user.id !== id))
+	}
+
+	const updateUser = (id, updatedUser) => {
+		setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+	}
+
+	const editUser = user => {
+		setViewMode("edit")
+		setCurrentUser({ id: user.id, name: user.name, username: user.username })
+	}
+
+	return (
+			 <div className="flex-row">
+				<div className="flex-large">
+				{(userViewMode == "edit") ? (
+						<Fragment>
+							<h3>Edit user</h3>
+							<UserForm
+								editing={true}
+								setViewMode={setViewMode}
+								currentUser={currentUser}
+								updateUser={updateUser}
+							/>
+				</Fragment> ) : 
+					(userViewMode == "new") ? 
+						(
+							<Fragment>
+								<h3>Create user</h3>
+								<UserForm
+									editing={false}
+									setViewMode={setViewMode}
+									currentUser={initialFormState}
+									updateUser={updateUser}
+								/>
+					</Fragment> ) :
+						(
+							<Fragment>
+								<div className="centered">
+									<h3>View users</h3>
+								</div>
+								<div style={{ display: "flex", justifyContent: "flex-end" }}>
+									<button
+										onClick={() => addUser()}
+										className="button muted-button"
+									>
+										Add New User
+									</button>
+								</div>
+								<UserTable users={users} editRow={editUser} deleteUser={deleteUser} />
+						</Fragment> )}
+				</div>
+			</div>
+	)
+}
+
+export default Users
